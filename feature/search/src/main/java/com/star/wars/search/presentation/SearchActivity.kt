7875cb9dd485901b7219349494e717b1e18e5a6e
Adit lal <adit.lal@gojek.com>
2021-05-22 22:24:21 +0530

    Adds search as a feature module

---
+++ feature/search/src/main/java/com/star/wars/search/presentation/SearchActivity.kt
@@ -0,0 +1,7 @@
+package com.star.wars.search.presentation
+
+import com.star.wars.common.base.BaseActivity
+import com.star.wars.common.base.ViewState
+
+class SearchActivity : BaseActivity<ViewState>() {
+}
