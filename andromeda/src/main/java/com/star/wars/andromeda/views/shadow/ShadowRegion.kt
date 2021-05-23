34dfd0f2a317bad349b2eab3a36ec242297980c6
Adit lal <adit.lal@gojek.com>
2021-05-23 16:40:11 +0530

    Adds a Shadow data class for Shadow view

---
+++ andromeda/src/main/java/com/star/wars/andromeda/views/shadow/ShadowRegion.kt
@@ -0,0 +1,8 @@
+package com.star.wars.andromeda.views.shadow
+
+internal data class ShadowRegion(
+        val left: Boolean = true,
+        val top: Boolean = true,
+        val right: Boolean = true,
+        val bottom: Boolean = true
+)
