0be2eb94a87b3ad10ee236ca82c1f0ce9fa6f02c
Adit lal <adit.lal@gojek.com>
2021-05-23 18:03:40 +0530

    Adds Search Activity UI bindings

--- feature/search/src/main/java/com/star/wars/search/presentation/SearchActivity.kt
+++ feature/search/src/main/java/com/star/wars/search/presentation/SearchActivity.kt
@@ -1,16 +1,24 @@
 package com.star.wars.search.presentation
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
+import com.star.wars.andromeda.Color
+import com.star.wars.andromeda.toColorToken
+import com.star.wars.andromeda.tokens.icon_dynamic_default
+import com.star.wars.andromeda.views.assets.icon.Icon
+import com.star.wars.andromeda.views.navbar.AndromedaNavBar
 import com.star.wars.common.addTo
 import com.star.wars.common.base.BaseActivity
 import com.star.wars.common.viewBinding
+import com.star.wars.search.R
 import com.star.wars.search.databinding.ActivitySearchBinding
 import com.star.wars.search.domain.SearchState
 import com.star.wars.search.domain.SearchViewModel
-import com.star.wars.search.presentation.SearchEvent.*
+import com.star.wars.search.presentation.SearchEvent.SearchErrorEvent
+import com.star.wars.search.presentation.SearchEvent.SearchResultsFetched
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchActivity : BaseActivity<SearchState>() {
 
     private val binding by viewBinding(ActivitySearchBinding::inflate)
@@ -20,7 +28,20 @@ class SearchActivity : BaseActivity<SearchState>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initNavBar()
         setUpHandlers()
+    }
+
+    private fun initNavBar() {
+        with(binding.navBarLayout.navBar) {
+            setTitle(resources.getString(R.string.navbar_search_title))
+            setSubtitle(resources.getString(R.string.navbar_search_subtitle))
+            showNavigationIcon(Icon.NAVIGATION_BACK, listener = {
+                finish()
+            })
+            setNavigationLogo(Icon.UNIVERSE, icon_dynamic_default)
+            divider = AndromedaNavBar.Divider.LINE
+        }
     }
 
     private fun setUpHandlers() {
