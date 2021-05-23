99794d50de7313bec1e742b08e28912a5d0ad9c6
Adit lal <adit.lal@gojek.com>
2021-05-23 16:38:11 +0530

    Adds Plain Divider impl

---
+++ andromeda/src/main/java/com/star/wars/andromeda/views/divider/internal/PlainDivider.kt
@@ -0,0 +1,22 @@
+package com.star.wars.andromeda.views.divider.internal
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import com.star.wars.andromeda.extensions.toPxInt
+import com.star.wars.andromeda.tokens.fill_mute_primary
+
+internal class PlainDivider(context: Context) : Divider {
+
+    override val height = 1f.toPxInt(context)
+
+    override val dividerLineHeight = height
+
+    override val paint = Paint(ANTI_ALIAS_FLAG).apply {
+        color = context.fill_mute_primary
+        style = Paint.Style.STROKE
+        strokeWidth = height.toFloat()
+    }
+
+    override val backgroundPaint: Paint? = null
+}
