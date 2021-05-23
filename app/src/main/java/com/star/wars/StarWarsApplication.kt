2aa0ee13c3cf33c4ba0c22cbe08aea47bb930d11
Adit lal <adit.lal@gojek.com>
2021-05-23 14:50:24 +0530

    Adds Andromeda Theme as default

--- app/src/main/java/com/star/wars/StarWarsApplication.kt
+++ app/src/main/java/com/star/wars/StarWarsApplication.kt
@@ -1,9 +1,11 @@
 package com.star.wars
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import com.orhanobut.logger.PrettyFormatStrategy
+import com.star.wars.andromeda.theme.AndromedaThemeLifecycleCallbacks
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -13,6 +15,12 @@ class StarWarsApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         plantTimberTree()
+        initComponents()
+    }
+
+    private fun initComponents() {
+        AndromedaThemeLifecycleCallbacks(this)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
     }
 
     private fun plantTimberTree() {
