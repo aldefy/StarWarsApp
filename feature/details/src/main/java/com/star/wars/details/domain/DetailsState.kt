6ad14fb5d6f19bf5ccebb1dbe949cb9f18362f8b
Adit lal <adit.lal@gojek.com>
2021-05-26 15:24:16 +0530

    Converts Details UpdateState to capture BaseComponent data

--- feature/details/src/main/java/com/star/wars/details/domain/DetailsState.kt
+++ feature/details/src/main/java/com/star/wars/details/domain/DetailsState.kt
@@ -1,13 +1,13 @@
 package com.star.wars.details.domain
 
-import com.star.wars.andromeda.views.list.ComponentData
+import com.star.wars.andromeda.views.list.BaseComponentData
 import com.star.wars.common.base.ViewState
 
 sealed class DetailsState : ViewState {
-    data class UpdateCharacterDetails(val data: List<ComponentData>) : DetailsState()
-    data class UpdatePlanetDetails(val data: List<ComponentData>) : DetailsState()
-    data class UpdateSpeciesDetails(val data: List<ComponentData>) : DetailsState()
-    data class UpdateFilmsDetails(val data: List<ComponentData>) : DetailsState()
+    data class UpdateCharacterDetails(val data: List<BaseComponentData>) : DetailsState()
+    data class UpdatePlanetDetails(val data: List<BaseComponentData>) : DetailsState()
+    data class UpdateSpeciesDetails(val data: List<BaseComponentData>) : DetailsState()
+    data class UpdateFilmsDetails(val data: List<BaseComponentData>) : DetailsState()
     data class Error(val errorMessage: String) : DetailsState()
     object ShowLoading : DetailsState()
     object HideLoading : DetailsState()
